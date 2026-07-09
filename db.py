41d0a1fcd507d42e9588be32f65eec0b991d18ba
Neha <nehasolai@gmail.com>
2026-07-09 10:52:47 +0530

    Rename database env variables

--- db.py
+++ db.py
@@ -9,14 +9,14 @@ print("Loading:", dotenv_path)
 
 load_dotenv(dotenv_path=dotenv_path, override=True)
 
-print("USERNAME =", os.getenv("USERNAME"))
+print("USERNAME =", os.getenv("DB_USERNAME"))
 
 conn = (
     "DRIVER={ODBC Driver 17 for SQL Server};"
     f"SERVER=tcp:{os.getenv('SERVER')},1433;"
     f"DATABASE={os.getenv('DATABASE')};"
-    f"UID={os.getenv('USERNAME')};"
-    f"PWD={os.getenv('PASSWORD')};"
+    f"UID={os.getenv('DB_USERNAME')};"
+    f"PWD={os.getenv('DB_PASSWORD')};"
     "Encrypt=yes;"
     "TrustServerCertificate=no;"
     "Connection Timeout=30;"
