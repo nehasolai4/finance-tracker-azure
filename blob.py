c459051fa1cf7c19203693a4aecdaae5b357ea91
Neha <nehasolai@gmail.com>
2026-07-09 11:12:31 +0530

    chnages made to blob.py

--- blob.py
+++ blob.py
@@ -4,16 +4,14 @@ from azure.storage.blob import BlobServiceClient
 
 load_dotenv()
 
-connection_string = os.getenv("BLOB_CONNECTION_STRING")
-container_name = os.getenv("CONTAINER_NAME")
-
-blob_service_client = BlobServiceClient.from_connection_string(connection_string)
-container_client = blob_service_client.get_container_client(container_name)
-
 def upload_file(file):
+    connection_string = os.getenv("BLOB_CONNECTION_STRING")
+    container_name = os.getenv("CONTAINER_NAME")
+
+    blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+    container_client = blob_service_client.get_container_client(container_name)
 
     blob_client = container_client.get_blob_client(file.filename)
-
     blob_client.upload_blob(file, overwrite=True)
 
     return blob_client.url
